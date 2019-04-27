994471decb935f08b123392fdf662fc1db67613b
Devon <devondodgson@gmail.com>
2019-04-27 13:46:39 -0700

    Initial Commit

--- Midnight Melody/ViewController.swift
+++ Midnight Melody/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    let soundArray = ["note1", "note2", "note3", "note5", "note6"]
+    
+    var selectedSoundFileName = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        playSound()
+        
+    }
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            
+            audioPlayer.play()
+        }
+        catch {
+            print(error)
+            
+        }
+        
     }
 
 
